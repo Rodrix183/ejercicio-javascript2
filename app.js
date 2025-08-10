7f6a8fa64f403af4fc163e756815a3c768184d25
Rodrigo Velásquez <38990525+Rodrix183@users.noreply.github.com>
2025-08-10 16:02:24 -0600

    Asignar notas a variables

---
+++ app.js
@@ -0,0 +1,8 @@
+//Boton Calcular
+document.getElementById("btnCalcular").addEventListener("click", function () {
+    //Asignar notas de los inputs
+    let nota1 = document.getElementById("nota1").value;
+    let nota2 = document.getElementById("nota2").value;
+    let nota3 = document.getElementById("nota3").value;
+
+}
